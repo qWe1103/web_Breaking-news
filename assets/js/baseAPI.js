9b3e6dfed11af5162a0c40a2f4f3bfb8092a6b66
whb <2994048040@qq.com>
2024-08-16 23:02:35 +0800

    完成了主页开发内容

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,17 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    // 统一为有权限的接口，设置headers请求头
+    if (options.headers.indexOf(/my/) !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+    // 不论成功还是失败，最终都会调用complete回调函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token');
+            // 强制跳转到登陆页面
+            location.href = '/login.html';
+        }
+    }
 })
